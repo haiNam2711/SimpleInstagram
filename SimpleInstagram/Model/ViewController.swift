b5d5e31082feaf48cdb2c90e9c8a092dfb842715
hnam2711 <namcuong2711@gmail.com>
2024-02-25 22:46:35 +0700

    Init ASDK Project

---
+++ SimpleInstagram/Model/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SimpleInstagram
+//
+//  Created by Hai Nam on 25/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+}
+
